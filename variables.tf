cbe57e6a39452c36378a2f0ae171387e1ecf2c15
vasavi <tvasu370@gmail.com>
2025-01-14 21:07:14 +0530

    added validation block

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
 variable "ami-id"{
+    type = string
 #    default = "ami-09c813fb71547fc4f"
 }
 
 variable "instance_type" {
-#  default = "t3.micro" 
+    type = string
+    default = "t3.micro" 
+    # we are restricting users dont use beyond these- t3.micro, t3.medium, t3.small
+    validation {
+        condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+        error_message = "instance_type can only be t3.micro, t3.small, t3.medium"
+    }
 } 
 
 variable "sg_ids"{
